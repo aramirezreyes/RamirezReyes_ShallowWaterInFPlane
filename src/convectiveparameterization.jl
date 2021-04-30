0889a772a14ee2d45b8aee73cc10db49c88972ac
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-04-29 23:07:57 -0700

    Created functions and tests for parameterization

--- src/convectiveparameterization.jl
+++ src/convectiveparameterization.jl
@@ -1,11 +1,12 @@
-function update_convective_events!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold)
-    for ind in eachindex(h)
+function update_convective_events!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Hx,Hy,Hz)
+    #@show typeof(h)
+    @inbounds for ind in CartesianIndices(isconvecting)
         if isconvecting[ind]
             ((t - convection_triggered_time[ind]) < τ_convec) && continue
             isconvecting[ind] = false
             convection_triggered_time[ind] = 0.0
-        elseif !isconvecting[ind] && (h[ind] <= h_threshold)
-            @info "Inside "
+        elseif !isconvecting[ind] && (h[ind[1], ind[2], 1 ] <= h_threshold)
+     #       @info "Inside "
             isconvecting[ind] = true
             convection_triggered_time[ind] = t
         end
@@ -14,7 +15,55 @@ function update_convective_events!(isconvecting,convection_triggered_time,h,t,τ
 end
 
 
-function heat_at_point(i,j,k,grid,h)
-#    element_at_right(i) = 
-    @show grid
+function heat(t,distance_from_conv_centersq,conv_time_triggered,q0,τ_c,R2,A0)
+    deltat   = t - conv_time_triggered
+    quotient = 2.0 * (deltat - τ_c/2.0)/(τ_c)
+    q        = q0*(1.0 - quotient*quotient)*(1.0 - (distance_from_conv_centersq / R2))
+#    if (q/ (τ_c*A0) ) < 0
+#        @info "Heating is negative. This is not right."
+#    end
+    return  q / (τ_c*A0)
+end
+
+@inline nth_neighbor(i,n,N) = mod1(i + n,N)
+
+    
+
+function heat_at_point(i,j,k,clock,τ_c,convective_radius,isconvecting,convection_triggered_time,q0,Δx,Δy,Nx,Ny,numelements_to_traverse_x,numelements_to_traverse_y)
+    forcing = 0.0
+    R2 = convective_radius * convective_radius
+    A0 = pi*R2
+    @inbounds for neigh_y_id in (-numelements_to_traverse_y:numelements_to_traverse_y)
+        y_distancesq = neigh_y_id * neigh_y_id* Δy * Δy
+        idy = nth_neighbor(j,neigh_y_id,Ny)
+        @inbounds for neigh_x_id in (-numelements_to_traverse_x:numelements_to_traverse_x)
+            x_distancesq = neigh_x_id * neigh_x_id * Δx * Δx
+            distance_from_conv_centersq = x_distancesq + y_distancesq #takes time
+            if distance_from_conv_centersq < R2
+                idx = nth_neighbor(i,neigh_x_id,Nx)
+                if isconvecting[idx , idy]
+                    forcing += heat(clock.time,distance_from_conv_centersq,convection_triggered_time[idx,idy],q0,τ_c,R2,A0)
+                end
+            end
+        end
+    end 
+    return forcing
+end
+
+
+function model_forcing(i,j,k,grid,clock,model_fields,parameters)
+    forcing =  heat_at_point(i,j,k,clock,
+                       parameters.τ_c,
+                       parameters.R,
+                       parameters.isconvecting,
+                       parameters.convection_triggered_time,
+                             parameters.q0,
+                             grid.Δx,
+                             grid.Δy,
+                             grid.Nx,
+                             grid.Ny,
+                             parameters.numelements_to_traverse_x,
+                             parameters.numelements_to_traverse_y,
+                      )
+
 end
--- test/convectiveparameterization.jl
+++ test/convectiveparameterization.jl
@@ -1,4 +1,12 @@
+using Oceananigans
+using BenchmarkTools
 
+Lx, Ly, Lz = 1.0e2, 1.0e2, 40
+Nx, Ny = 10, 10
+
+grid = RegularRectilinearGrid(size = (Nx, Ny, 1),
+                            x = (0, Lx), y = (0, Ly), z = (0, Lz),
+                              topology = (Periodic, Periodic, Bounded))
 h_c = 40.0
 τ_c = 5.0
 h = 40.5ones(10,10)
@@ -8,18 +16,31 @@ convection_triggered_time = zeros(10,10)
 t1 = 10.
 t2 = t1 + τ_c/2
 t3 = t1 + τ_c + 0.5
-
+R2 = 20
+q0 = 5
+struct TestClock{T}
+    time :: T
+end
+clock = TestClock(t2)
 @testset "Detect convecting events" begin
     @test convection_triggered_time[5,5] == 0.0
-    update_convective_events!(isconvecting,convection_triggered_time,h,t1,τ_c,h_c)
+    update_convective_events!(isconvecting,convection_triggered_time,h,t1,τ_c,h_c,0,0,0)
     @test only(findall(isconvecting)) == CartesianIndex(5,5)
     @test convection_triggered_time[5,5] == t1
-    update_convective_events!(isconvecting,convection_triggered_time,h,t2,τ_c,h_c)
+    update_convective_events!(isconvecting,convection_triggered_time,h,t2,τ_c,h_c,0,0,0)
     @test only(findall(isconvecting)) == CartesianIndex(5,5)
     @test convection_triggered_time[5,5] == t1
-    update_convective_events!(isconvecting,convection_triggered_time,h,t3,τ_c,h_c)
+    update_convective_events!(isconvecting,convection_triggered_time,h,t3,τ_c,h_c,0,0,0)
     @test isempty(findall(isconvecting)) 
     @test convection_triggered_time[5,5] == 0
 end
 
+
+@testset "Heating function" begin
+    isconvecting = zeros(Bool,10,10)
+    h[5,5] = 39.0
+    convection_triggered_time = zeros(10,10)
+    @test heat_at_point(0,0,1,grid,clock,h,τ_c,h_c,isconvecting,convection_triggered_time,R2,q0) == 0.0
+
+end
 
