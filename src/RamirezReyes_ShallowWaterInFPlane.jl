8fd42fce035f03c1772b9351637ad851f86c295e
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-05 08:55:53 -0700

    Duplicated code but now for boundary layer

--- src/RamirezReyes_ShallowWaterInFPlane.jl
+++ src/RamirezReyes_ShallowWaterInFPlane.jl
@@ -2,16 +2,28 @@ module RamirezReyes_ShallowWaterInFPlane
 using Reexport
 @reexport using Printf
 @reexport using Oceananigans
-@reexport using CUDA: @cuda, blockIdx, blockDim, threadIdx, gridDim
+@reexport using CUDA: @cuda, blockIdx, blockDim, threadIdx, gridDim, launch_configuration
 @reexport using DrWatson
+import CUDA
 include(joinpath(@__DIR__,"../src/convectiveparameterization.jl"))
+include(joinpath(@__DIR__,"../src/convectiveparameterization_boundarylayer.jl"))
 include(joinpath(@__DIR__,"../src/arrayutils.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_debug_run.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_15d_simulation.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_debug_run_uppertropo.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_15d_simulation_uppertropo.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation_uppertropo.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation_bl.jl"))
 
-export update_convective_events!, model_forcing, u_damping, v_damping, run_shallow_simulation, fill_heating_stencil!, run_shallow_simulation_100d, run_shallow_simulation_debug
-
+export u_damping,
+    v_damping,
+    fill_heating_stencil!,
+    update_convective_events!,
+    model_forcing,
+    update_convective_events_bl!,
+    model_forcing_bl,
+    run_shallow_simulation_15d_uppetropo,
+    run_shallow_simulation_100d_uppertropo,
+    run_shallow_simulation_uppertropo_debug,
+    run_shallow_simulation_100d_bl
 
 end #module
 
