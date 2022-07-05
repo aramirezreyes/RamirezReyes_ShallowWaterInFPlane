ab651256d1974392fc1a127a18c156493265bb2f
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-05 12:49:01 -0700

    added boundary layer option n parameter list

--- scripts/create_parameters_and_submit_simulations.jl
+++ scripts/create_parameters_and_submit_simulations.jl
@@ -7,27 +7,32 @@ using DrWatson
 
 parameter_space = Dict(
     "architecture" => "GPU",
-    "f" => 0.0,# coriolis parameter5e-4 #5e-4
-    "g" => 10,#gravitational acceleration 9.8
-    "convec_t" => (28800, 10800), #convective time scale
-    "h_c"=> 40, #convection_triggering height
-    "q0"    => (5.0e11,1e11,5e10,1e10),#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
-    "r" => (1.12/3)*1.0e-8,#radiative cooling rate
-    "convec_r"    => (30000,20000,10000), #convective radius
-    "relax_t" => 1.0/(2*86400.0), #relaxation timescale
-    "relax_h" => 39, #height to relax to
-    "Lx" => 1.5e6, #domain
-    "Ly" => 1.5e6,
-    "Lz" => 45,
-    "Nx" => 200,
-    "Ny" => 200
+    "f" => [0.0f0, 1f-3, 5f-4, 1f-4, 5f-5],# coriolis parameter5e-4 #5e-4
+    "g" => 10.0f0,#gravitational acceleration 9.8
+    "tauconvec" => [28800.0f0, 10800.0f0], #convective time scale
+    "hc"=> 40.0f0, #convection_triggering height
+    "q0"    => [5f10,1f10],#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
+    "r" => (1.12f0/3.0f0)*[1.0f-8, 1.0f-9],#radiative cooling rate
+    "rconvec"    => [30000.0f0,20000.0f0,10000.0f0], #convective radius
+    "taurelax" => 1.0/(2*86400.0), #relaxation timescale
+    "hrelax" => 39.0f0, #height to relax to
+    "Lx" => 6f6, #domain
+    "Ly" => 6f6,
+    "Lz" => 45.0f0,
+    "Nx" => 2000,
+    "Ny" => 2000,
+    "boundarylayer" => true,
 )
 
 
 dicts = dict_list(parameter_space)
 res = tmpsave(dicts)
-for r in res
-    println(r)
-    submit = `sbatch submit_simulations_sbatch.bash $r`
+for r in Iterators.partition(res,4)
+    arg1 = r[1]
+    arg2 = r[2]
+    arg3 = r[3]
+    arg4 = r[4]
+    submit = `sbatch submit_simulations_sbatch.bash $arg1 $arg2 $arg3 $arg4`
+    #submit = `bash print_jobs.bash $arg1 $arg2 $arg3 $arg4`
     run(submit)
 end
