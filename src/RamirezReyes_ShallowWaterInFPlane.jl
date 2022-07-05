6e87e2749ab4b24a4d87258a49bd5e2dd2774300
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-05 12:46:26 -0700

    Moved the boundary layer option to the same functions to avoid boilerplate

--- src/RamirezReyes_ShallowWaterInFPlane.jl
+++ src/RamirezReyes_ShallowWaterInFPlane.jl
@@ -8,22 +8,16 @@ import CUDA
 include(joinpath(@__DIR__,"../src/convectiveparameterization.jl"))
 include(joinpath(@__DIR__,"../src/convectiveparameterization_boundarylayer.jl"))
 include(joinpath(@__DIR__,"../src/arrayutils.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_debug_run_uppertropo.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_15d_simulation_uppertropo.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation_uppertropo.jl"))
-include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation_bl.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_debug_run.jl"))
+include(joinpath(@__DIR__,"../src/run_oceananigans_100d_simulation.jl"))
+
 
 export u_damping,
     v_damping,
     fill_heating_stencil!,
     update_convective_events!,
     model_forcing,
-    update_convective_events_bl!,
-    model_forcing_bl,
-    run_shallow_simulation_15d_uppetropo,
-    run_shallow_simulation_100d_uppertropo,
-    run_shallow_simulation_uppertropo_debug,
-    run_shallow_simulation_100d_bl
+    run_shallow_simulation_100d,
 
 end #module
 
--- src/convectiveparameterization.jl
+++ src/convectiveparameterization.jl
@@ -18,13 +18,13 @@ The version that receives architecture as first parameter is an interface for th
 Based on:
 Yang, D., and A. P. Ingersoll, 2013: Triggered Convection, Gravity Waves, and the MJO: A Shallow-Water Model. J. Atmos. Sci., 70, 2476–2486, https://doi.org/10.1175/JAS-D-12-0255.1.
 """
-function update_convective_events!(architecture :: GPU,isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny)
-    kernel = @cuda launch=false update_convective_events_gpu!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny)
+function update_convective_events!(architecture :: GPU,isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny; boundary_layer = true)
+    kernel = @cuda launch=false update_convective_events_gpu!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny,boundary_layer)
     config = launch_configuration(kernel.fun)
     threads = min(size(isconvecting,1), config.threads)
     blocks = cld(size(isconvecting,1), threads)
     CUDA.@sync begin
-        kernel(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny;threads, blocks)
+        kernel(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny,boundary_layer; threads, blocks)
     end
 end
 
@@ -56,8 +56,10 @@ The version contains the implementation for GPU using CUDA.
 Based on:
 Yang, D., and A. P. Ingersoll, 2013: Triggered Convection, Gravity Waves, and the MJO: A Shallow-Water Model. J. Atmos. Sci., 70, 2476–2486, https://doi.org/10.1175/JAS-D-12-0255.1.
 """
-function update_convective_events_gpu!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny)
+function update_convective_events_gpu!(isconvecting,convection_triggered_time,h,t,τ_convec,h_threshold,Nx,Ny, boundary_layer = false)
     #@show typeof(h)
+
+    compare = boundary_layer ? (>=) : (<=)
 
     index_x = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     stride_x = gridDim().x * blockDim().x
@@ -70,7 +72,7 @@ function update_convective_events_gpu!(isconvecting,convection_triggered_time,h,
         for j in index_y:stride_y:Ny
             time_convecting = t - convection_triggered_time[i,j]
             needs_to_convect_by_time = isconvecting[i,j] && (time_convecting < τ_convec) #has been convecting less than τ_c?
-            needs_to_convect_by_height = (h[i,j] <= h_threshold)
+            needs_to_convect_by_height = compare(h[i,j] , h_threshold)
             will_start_convecting = needs_to_convect_by_height && iszero(needs_to_convect_by_time) #time needs be updated?
             isconvecting[i,j] = needs_to_convect_by_time || needs_to_convect_by_height 
             will_start_convecting && (convection_triggered_time[i,j] = t) #Update time only if new convective event
@@ -106,12 +108,13 @@ Tells you the index of your nth neighbor considering periodic boundaries.
 
 Yang, D., and A. P. Ingersoll, 2013: Triggered Convection, Gravity Waves, and the MJO: A Shallow-Water Model. J. Atmos. Sci., 70, 2476–2486, https://doi.org/10.1175/JAS-D-12-0255.1.
 """
-@inline function heat_at_point(i,j,k,current_time,τc,convective_radius,isconvecting,convection_triggered_time,q0,Δx2,Δy2,numelements_to_traverse, heating_stencil)
+@inline function heat_at_point(i,j,k,current_time,τc,convective_radius,isconvecting,convection_triggered_time,q0,Δx2,Δy2,numelements_to_traverse, heating_stencil, boundary_layer = false)
+    add_or_substract_mass = boundary_layer ? (-) : (+)
     forcing = 0.0
     @inbounds for neigh_j in (-numelements_to_traverse:numelements_to_traverse)
         @inbounds for neigh_i in (-numelements_to_traverse:numelements_to_traverse)
              if isconvecting[i + neigh_i , j + neigh_j]
-                 forcing += heat(current_time,convection_triggered_time[i + neigh_i,j + neigh_j],τc,heating_stencil[-neigh_i,-neigh_j])
+                 forcing = add_or_substract_mass( forcing , heat(current_time,convection_triggered_time[i + neigh_i,j + neigh_j],τc,heating_stencil[-neigh_i,-neigh_j]))
              end
         end
     end 
@@ -187,6 +190,8 @@ This is an interface with the correct signature to register the convective param
 It also adds the radiative cooling and the relaxation in the height field with a given timescale.
 """
 function model_forcing(i,j,k,grid,clock,model_fields,parameters)
+    boundary_layer = parameters.boundary_layer
+    radiative_cooling_rate = boundary_layer ? parameters.radiative_cooling_rate : -1*parameters.radiative_cooling_rate
     heat_at_point(i,j,k,clock.time,
                       parameters.τ_c,
                       parameters.R,
@@ -196,6 +201,6 @@ function model_forcing(i,j,k,grid,clock,model_fields,parameters)
                       parameters.Δx2,
                       parameters.Δy2,
                       parameters.nghosts,
-                      parameters.heating_stencil) - parameters.radiative_cooling_rate - (model_fields.h[i,j,k] - parameters.relaxation_height)*parameters.relaxation_parameter
+                      parameters.heating_stencil,parameters.boundary_layer) + radiative_cooling_rate - (model_fields.h[i,j,k] - parameters.relaxation_height)*parameters.relaxation_parameter
 end
 
