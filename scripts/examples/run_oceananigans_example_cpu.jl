1354364b0e12a3a09ac408d911dd38daa45cacf0
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2022-07-07 11:36:03 -0700

    Improved legibility

--- scripts/examples/run_oceananigans_example_cpu.jl
+++ scripts/examples/run_oceananigans_example_cpu.jl
@@ -10,28 +10,28 @@ function run_shallow_simulation(arch = "CPU")
     end
     @info "Using architecture: ", architecture
     #Setup physicsl parameters
-    f = 5e-4 #Coriolis
-    g = 9.8 #Gravity
-    τ_c = 10800.0 #Duration of convective events (in seconds)
-    h_c = 130.0 #Critical height that triggers convection (in meters)
-    heating_amplitude = 3e9 #The amplitude of the convective event (q0 in paper)
-    radiative_cooling_rate = (1.12/3)*1.0e-8 #The amplitude of the large scale forcing
-    convective_radius    = 30000.0 #Radius of convective event (in meters)
-    relaxation_parameter = 1.0/(2*86400) # 1/τ where tau is the relaxation timescale for friction and h recovery
-    relaxation_height = 129.0 #Target for the recovery of the h field
-    Lx = 1.5e6 #Size of the domain (in meters)
+    f = 5e-4                                  #Coriolis
+    g = 9.8                                   #Gravity
+    τ_c = 10800.0                             #Duration of convective events [s]
+    h_c = 130.0                               #Critical height that triggers convection [m]
+    heating_amplitude = 3e9                   #Amplitude of the convective event (q0 in paper)
+    radiative_cooling_rate = (1.12/3)*1.0e-8  #The amplitude of the large scale forcing
+    convective_radius    = 30000.0            #Radius of convective event [m]
+    relaxation_parameter = 1.0/(2*86400)      #1/τ where τ is the relaxation timescale for friction and h recovery
+    relaxation_height = 129.0                 #Target for the recovery of the h field
+    Lx = 1.5e6                                #Size of the domain [m]
     Ly = 1.5e6
-    Lz = 126.5 # A characteristic height
-    Nx = 500 #Number of points
+    Lz = 126.5                                # Acharacteristic height used in initialization
+    Nx = 500                                  #Number of points
     Ny = 500
-    boundary_layer = true #If true, convection is a mass sink, otherwise is false
+    boundary_layer = true                     #If true, convection is a mass sink, otherwise is false
     
     ## Setup other simulation prameters
     output_dir = datadir()
     output_filename = "example_run_"*arch*".nc"
-    save_every = 7200 #in seconds
-    simulation_length = 3 #in days
-    Δt = 5.0   #timestep in seconds
+    save_every = 7200                          #in seconds
+    simulation_length = 3                      #in days
+    Δt = 5.0                                   #timestep in seconds
 
     ####################################
     ##### Simulation setup below ######
@@ -43,21 +43,30 @@ function run_shallow_simulation(arch = "CPU")
     halo_indices = numelements_to_traverse-1
 
     grid = RectilinearGrid(architecture,size = (Nx, Ny),
-                           x = (0, Lx), y = (0, Ly),
-                           topology = (Periodic, Periodic, Flat), halo = (max(numelements_to_traverse,3), max(numelements_to_traverse,3)))
+        x = (0, Lx), y = (0, Ly),
+        topology = (Periodic, Periodic, Flat), 
+        halo = (max(numelements_to_traverse,3), max(numelements_to_traverse,3)))
 
     @info "Built grid successfully"
-    isconvecting  = CenterField(grid,Bool)
+
+    ## Fields needed for the convective parameterization
+    isconvecting               = CenterField(grid,Bool)
     convection_triggered_time  = CenterField(grid)
     ## Will create heating stencil with the spatial component
     q_stencil = CenterField(grid,Float64; indices=(-halo_indices:halo_indices,-halo_indices:halo_indices,:))
     fill_heating_stencil!(grid.architecture,q_stencil,heating_amplitude,grid_spacing_x,convective_radius^2)
-    parameters = (; isconvecting = isconvecting, convection_triggered_time, τ_c, h_c, nghosts = numelements_to_traverse - 1, radiative_cooling_rate , q0 = heating_amplitude, R = convective_radius, relaxation_parameter, relaxation_height, Δx2 = grid_spacing_x^2, Δy2 = grid_spacing_y^2, heating_stencil = q_stencil, boundary_layer)
+
+
+    parameters = (; isconvecting = isconvecting, convection_triggered_time, τ_c, h_c, 
+    nghosts = numelements_to_traverse - 1, radiative_cooling_rate , q0 = heating_amplitude,
+    R = convective_radius, relaxation_parameter, relaxation_height, Δx2 = grid_spacing_x^2,
+    Δy2 = grid_spacing_y^2, heating_stencil = q_stencil, boundary_layer
+    )
 
     #build forcing
     convec_forcing = Forcing(model_forcing,discrete_form=true,parameters = parameters)
-    u_forcing = Forcing(u_damping, parameters=relaxation_parameter, field_dependencies=:u)
-    v_forcing = Forcing(v_damping, parameters=relaxation_parameter, field_dependencies=:v)
+    u_forcing      = Forcing(u_damping, parameters=relaxation_parameter, field_dependencies=:u)
+    v_forcing      = Forcing(v_damping, parameters=relaxation_parameter, field_dependencies=:v)
 
     # Build the model
     model = if iszero(f)
@@ -88,19 +97,19 @@ end
 
 #### This shallow water model uses the fluxes as prognostic variables we will initialize with these functions
     uhⁱ(x, y, z) = 0.0 
-    h̄(x, y, z) = Lz + 4rand()
+    h̄(x, y, z)   = Lz + 4rand()
     set!(model, uh = uhⁱ, h = h̄)
 
 
     ### For output purposes we want the velocities and vorticity
     uh, vh, h = model.solution
-    u = uh / h
-    v = vh / h
+    u         = uh / h
+    v         = vh / h
 
     ## Build and compute mean vorticity discretely
-    ω = Field(∂x(v) - ∂y(u))
+    ω     = Field(∂x(v) - ∂y(u))
     diver = Field(∂x(u) + ∂y(v))
-    sp = @at (Center,Center, Center) sqrt(u^2 + v^2)
+    sp     = @at (Center,Center, Center) sqrt(u^2 + v^2)
     compute!(ω)
 
     simulation = Simulation(model; Δt = Δt , simulation_length)
