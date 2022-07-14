70911d667cb3533a045a1b1f4429bf663d06a64a
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-13 18:17:14 -0700

    Updated submission scripts

--- scripts/create_parameters_and_submit_simulations_largedomain_bl.jl
+++ scripts/create_parameters_and_submit_simulations_largedomain_bl.jl
@@ -7,24 +7,24 @@ using DrWatson
 
 parameter_space = Dict(
     "architecture" => "GPU",
-    "f" => [5f-3, 1f-4, 5f-5, 3f-5, 1f-5, 5f-6, 0.0],# coriolis parameter5e-4 #5e-4
-    "g" => 10.0f0,#gravitational acceleration 9.8
-    "tauconvec" => [28800.0f0, 10800.0f0], #convective time scale
-    "hc"=> 130.0f0, #convection_triggering height
-    "q0"    => [5f11,1f11,5f10,1f10],#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
-    "r" => (1.12f0/3.0f0)*[1.0f-8, 1.0f-9],#radiative cooling rate
-    "rconvec"    => [30000.0f0,20000.0f0,10000.0f0], #convective radius
+    "f" => [5e-3, 1e-4, 5e-5, 3e-5, 1e-5, 5e-6, 1e-6, 0.0],# coriolis parameter5e-4 #5e-4
+    "g" => 10.0,#gravitational acceleration 9.8
+    "tauconvec" => [28800.0, 10800.0], #convective time scale
+    "hc"=> 130.0, #convection_triggering height
+    "q0"    => [5e11,1e11,5e10,1e10,5e9,1e9],#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
+    "r" => (1.12/3.0)*[1.0e-8],#radiative cooling rate
+    "rconvec"    => [30_000.0,20_000.0,10_000.0, 5000.0], #convective radius
     "taurelax" => 1.0/(2*86400.0), #relaxation timescale
-    "hrelax" => 131.0f0, #height to relax to
-    "Lx" => 6f6, #domain
-    "Ly" => 6f6,
-    "Lz" => 126.0f0,
+    "hrelax" => 131.0, #height to relax to
+    "Lx" => 6e6, #domain
+    "Ly" => 6e6,
+    "Lz" => 126.0e0,
     "Nx" => 1500,
     "Ny" => 1500,
     "boundarylayer" => true,
     "simulation_length_in_days" => 100,
-    "output_interval_in_seconds" => 7200, 
-    "timestep_in_seconds" => 5,
+    "output_interval_in_seconds" => 7200.0, 
+    "timestep_in_seconds" => 20.0,
 )
 
 
--- scripts/submit_simulations_sbatch.bash
+++ scripts/submit_simulations_sbatch.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH --account m1517_g
-#SBATCH --qos=regular
-#SBATCH --time=02:00:00
+#SBATCH --account m1517
+#SBATCH --qos=regular_ss11
+#SBATCH --time=08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --constraint=gpu
@@ -23,8 +23,8 @@ echo $2
 echo $3
 echo $4
 
-srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=16G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $1 &
-srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=16G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $2 &
-srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=16G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $3 &
-srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=16G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $4 &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=50G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $1 &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=50G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $2 &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=50G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $3 &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=50G /global/u2/a/aramreye/Software/julia-1.8.0-rc1/bin/julia --project=@. read_parameter_file_and_launch_simulation.jl $4 &
 wait
