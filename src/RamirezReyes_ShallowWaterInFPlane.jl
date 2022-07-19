e04f6fa6d32708e14aa1e7523f675d0732ff418e
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-19 14:03:47 -0700

    Added posibility of relaxing height to its mean

--- src/RamirezReyes_ShallowWaterInFPlane.jl
+++ src/RamirezReyes_ShallowWaterInFPlane.jl
@@ -5,14 +5,20 @@ using Reexport
 @reexport using CUDA: @cuda, blockIdx, blockDim, threadIdx, gridDim, launch_configuration
 @reexport using LoopVectorization: @turbo, @tturbo
 @reexport using DrWatson
+@reexport using Statistics: mean
 import CUDA
 
 include(joinpath(@__DIR__,"../src/convectiveparameterization.jl"))
+include(joinpath(@__DIR__,"../src/helperfunctions.jl"))
 include(joinpath(@__DIR__,"../src/run_oceananigans_debug_run.jl"))
 include(joinpath(@__DIR__,"../src/run_oceananigans_simulation.jl"))
 
 
-export u_damping,
+export update_convective_helper_arrays,
+compute_nghosts,
+shorten_names,
+short_parameter_names,
+u_damping,  
     v_damping,
     fill_heating_stencil!,
     update_convective_events!,
