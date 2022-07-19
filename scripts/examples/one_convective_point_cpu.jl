ca28a92a204cfa708dacdbbde21c8d214e49a3a4
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-19 14:23:00 -0700

    Started to modify examples so that they use te same simulation function

--- scripts/examples/one_convective_point_cpu.jl
+++ scripts/examples/one_convective_point_cpu.jl
@@ -5,115 +5,31 @@
 # ΔH = 1/3 q0 
 
 
-function run_shallow_simulation(arch ; boundary_layer = true)
+### This is currently not working because initialization has random points
 
-    architecture = if arch == "CPU"
-        CPU()
-    elseif arch == "GPU"
-        GPU()
-    end
-    g = 0.0
-    τ_c = 1000.0
-    h_c = 40.0
-    heating_amplitude = 1e9
-    radiative_cooling_rate = 0.0
-    convective_radius    = 10_000.0
-    relaxation_parameter = 0
-    relaxation_height = 45.0
-    Lx = 30_000
-    Ly = 30_000
-    Lz = boundary_layer ? 39 : 41
-    Nx = 100
-    Ny = 100
-    simulation_length = 2000 / 86400
-    timestep = 20.0
+parameters = Dict(
+    "architecture" => "CPU",
+    "f" => 0.0,# coriolis parameter5e-4 #5e-4
+    "g" => 0.0,#gravitational acceleration 9.8
+    "convection_timescale" => 1000.0, #convective time scale
+    "convection_critical_height"=> 40.0, #convection_triggering height
+    "heating_amplitude"    => 1.0e9,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
+    "large_scale_forcing" => 0.0,#radiative cooling rate
+    "convective_radius"    => 10_000, #convective radius
+    "relaxation_parameter" => 0.0, #relaxation timescale
+    "relaxation_height" => nothing, #height to relax to, if nothing, h relax to its mean
+    "Lx" => 30000, #domain
+    "Ly" => 30000,
+    "Lz" => 39.0,
+    "Nx" => 100,
+    "Ny" => 100,
+    "boundary_layer" => false, #if true Lz=30 otherwise Lz = 41
+    "simulation_length_in_days" => 2000 / 86400,
+    "output_interval_in_seconds" => 20, 
+    "timestep_in_seconds" => 20,
+)
 
-    grid_spacing_x = Lx ÷ Nx #These two need to be equal for x and y!
-    grid_spacing_y = Ly ÷ Ny
+using RamirezReyes_ShallowWaterInFPlane
 
-    numelements_to_traverse = Int(convective_radius ÷ grid_spacing_x)
-    halo_indices = numelements_to_traverse-1
-
-    grid = RectilinearGrid(architecture,size = (Nx, Ny),
-                            x = (0, Lx), y = (0, Ly),
-                            topology = (Periodic, Periodic, Flat), halo = (max(numelements_to_traverse,3), max(numelements_to_traverse,3)))
-
-    isconvecting  = CenterField(grid,Bool)
-    convection_triggered_time  = CenterField(grid)
-    q_stencil = CenterField(grid,Float64; indices=(-halo_indices:halo_indices,-halo_indices:halo_indices,:))
-    fill_heating_stencil!(grid.architecture,q_stencil,heating_amplitude,grid_spacing_x,convective_radius^2)
-
-
-    parameters = (; isconvecting = isconvecting, convection_triggered_time, τ_c, h_c, nghosts = numelements_to_traverse - 1, radiative_cooling_rate , q0 = heating_amplitude, R = convective_radius, relaxation_parameter, relaxation_height, Δx2 = grid_spacing_x^2, Δy2 = grid_spacing_y^2, heating_stencil = q_stencil, boundary_layer)
-
-    convec_forcing = Forcing(model_forcing,discrete_form=true,parameters = parameters)
-
-    model = ShallowWaterModel(;grid = grid,
-    timestepper=:RungeKutta3,
-    momentum_advection=WENO5(grid=grid),
-    mass_advection=WENO5(grid=grid),
-    tracer_advection=WENO5(grid=grid),
-    gravitational_acceleration=g,
-    forcing=(h=convec_forcing,)
-    )
-
-
-    uhⁱ(x, y, z) = 0.0 
-    function h̄(x, y, z)
-        if x == grid.xᶜᵃᵃ[Nx ÷ 2] && y == grid.yᵃᶜᵃ[Ny ÷ 2]
-            return boundary_layer ? 41 : 39
-        else
-            return Lz 
-        end
-    end
-    uh, vh, h = model.solution
-
-   
-    ## Build velocities
-    u = uh / h
-    v = vh / h
-
-    set!(model, uh = uhⁱ, h = h̄)
-    initial_state = deepcopy(model.solution)
-
-    simulation = Simulation(model, Δt = timestep, stop_time = 86400.0*simulation_length)
-
-    function update_convective_helper_arrays(sim, parameters)
-        p = parameters
-        m = sim.model
-        update_convective_events!(m.architecture,p.isconvecting,p.convection_triggered_time,m.solution.h,
-                                m.clock.time,p.τ_c,p.h_c,m.grid.Nx,m.grid.Ny, p.boundary_layer)
-        Oceananigans.BoundaryConditions.fill_halo_regions!(p.isconvecting, m.architecture)
-        Oceananigans.BoundaryConditions.fill_halo_regions!(p.convection_triggered_time, m.architecture)
-
-    end
-
-
-     function progress(sim)
-         m = sim.model
-         @info(@sprintf("Iter: %d, time: %.1f, Δt: %.1f, max|h|: %.2f, min|h|: %.2f",
-                     m.clock.iteration, m.clock.time,
-                     sim.Δt, maximum(m.solution.h), minimum(m.solution.h)))
-             end
-
-
-        simulation.output_writers[:fields] =
-     NetCDFOutputWriter(
-         model,
-         (h = h , v = v , u = u, isconvecting = isconvecting ),
-         dir = datadir(),
-         filename = "validation.nc",
-         schedule = IterationInterval(1),
-         overwrite_existing = true)
-    
-
-
-        simulation.callbacks[:progress] = Callback(progress, IterationInterval(1))
-
-    simulation.callbacks[:update_convective_helper_arrays] = Callback(update_convective_helper_arrays, IterationInterval(1); parameters)
-
-    run!(simulation)
-
-end
-
-run_shallow_simulation("CPU")
+run_shallow_simulation(parameters)
+ 
--- src/run_oceananigans_debug_run.jl
+++ src/run_oceananigans_debug_run.jl
@@ -10,50 +10,37 @@ function run_shallow_simulation_debug(arch; ultrashort = false)
     end
     @info "Using architecture: ", architecture
     #Setup physicsl parameters
-    f = 5e-4 #Coriolis
-    g = 9.8 #Gravity
-    τ_c = 10800.0 #Duration of convective events (in seconds)
-    h_c = 130.0 #Critical height that triggers convection (in meters)
-    heating_amplitude = 3e9 #The amplitude of the convective event (q0 in paper)
-    radiative_cooling_rate = (1.12/3)*1.0e-8 #The amplitude of the large scale forcing
-    convective_radius    = 30000.0 #Radius of convective event (in meters)
-    relaxation_parameter = 1.0/(2*86400) # 1/τ where tau is the relaxation timescale for friction and h recovery
-    relaxation_height = 131.0 #Target for the recovery of the h field
-    Lx = 1.5e6 #Size of the domain (in meters)
-    Ly = 1.5e6
-    Lz = 126.5 # A characteristic height
-    Nx = 500 #Number of points
-    Ny = 500
-    boundary_layer = true #If true, convection is a mass sink, otherwise is false
+    parameters_dict = Dict{String, Union{Nothing, Real}}(
+    "f" => 5e-4, #Coriolis
+    "g" => 9.8, #Gravity
+    "convection_timescale" => 10800.0, #Duration of convective events (in seconds)
+    "convection_critical_height" => 130.0, #Critical height that triggers convection (in meters)
+    "heating_amplitude" => 3e9, #The amplitude of the convective event (q0 in paper)
+    "large_scale_forcing" => (1.12/3)*1.0e-8, #The amplitude of the large scale forcing
+    "convective_radius"    => 30000.0, #Radius of convective event (in meters)
+    "relaxation_parameter" => 1.0/(2*86400), # 1/τ where tau is the relaxation timescale for friction and h recovery
+    "relaxation_height" => nothing, #131.0 #Target for the recovery of the h field if nothing it will relax to the mean
+    "Lx" => 1.5e6, #Size of the domain (in meters)
+    "Ly" => 1.5e6,
+    "Lz" => 126.5, # A characteristic height
+    "Nx" => 500, #Number of points
+    "Ny" => 500,
+    "boundary_layer" => true #If true, convection is a mass sink, otherwise is false
+    )
     
-
-    
-
-    grid_spacing_x = Lx ÷ Nx #These two need to be equal for x and y!
-    grid_spacing_y = Ly ÷ Ny
-
-    numelements_to_traverse = Int(convective_radius ÷ grid_spacing_x)
-    halo_indices = numelements_to_traverse-1
-
-    grid = RectilinearGrid(architecture,size = (Nx, Ny),
-                           x = (0, Lx), y = (0, Ly),
-                           topology = (Periodic, Periodic, Flat), halo = (max(numelements_to_traverse,3), max(numelements_to_traverse,3)))
+    nghosts = compute_nghosts(parameters_dict)
+    grid = RectilinearGrid(architecture,size = (parameters_dict["Nx"], parameters_dict["Ny"]),
+                           x = (0, parameters_dict["Lx"]), y = (0, parameters_dict["Ly"]),
+                           topology = (Periodic, Periodic, Flat), halo = (nghosts, nghosts))
 
     @info "Built grid successfully"
-    isconvecting  = CenterField(grid,Bool)
-    convection_triggered_time  = CenterField(grid)
-    ## Will create heating stencil with the spatial component
-    q_stencil = CenterField(grid,Float64; indices=(-halo_indices:halo_indices,-halo_indices:halo_indices,:))
 
-    fill_heating_stencil!(grid.architecture,q_stencil,heating_amplitude,grid_spacing_x,convective_radius^2)
-
-    parameters = (; isconvecting = isconvecting, convection_triggered_time, τ_c, h_c, nghosts = numelements_to_traverse - 1, radiative_cooling_rate , q0 = heating_amplitude, R = convective_radius, relaxation_parameter, relaxation_height, Δx2 = grid_spacing_x^2, Δy2 = grid_spacing_y^2, heating_stencil = q_stencil, boundary_layer)
-
+    isconvecting, mean_h, parameters = build_convective_parameterization_tools(grid, parameters_dict)
 
     #build forcing
     convec_forcing = Forcing(model_forcing,discrete_form=true,parameters = parameters)
-    u_forcing = Forcing(u_damping, parameters=relaxation_parameter, field_dependencies=:u)
-    v_forcing = Forcing(v_damping, parameters=relaxation_parameter, field_dependencies=:v)
+    u_forcing = Forcing(u_damping, parameters=parameters.relaxation_parameter, field_dependencies=:u)
+    v_forcing = Forcing(v_damping, parameters=parameters.relaxation_parameter, field_dependencies=:v)
 
     ## Build the model
 
@@ -62,13 +49,13 @@ function run_shallow_simulation_debug(arch; ultrashort = false)
                             momentum_advection=WENO5(grid=grid),
                             mass_advection=WENO5(grid=grid),
                             tracer_advection=WENO5(grid=grid),
-                            gravitational_acceleration=g,
-                            coriolis=FPlane(f=f),
+                            gravitational_acceleration=parameters_dict["g"],
+                            coriolis=FPlane(f=parameters_dict["f"]),
                             forcing=(h=convec_forcing,u = u_forcing, v = v_forcing)
                             )
 
     uhⁱ(x, y, z) = 0.0 #uⁱ(x, y, z) * hⁱ(x, y, z)
-    h̄(x, y, z) = Lz + 4rand()
+    h̄(x, y, z) = parameters_dict["Lz"] + 4rand()
     uh, vh, h = model.solution
 
     ## Build velocities
@@ -85,6 +72,7 @@ function run_shallow_simulation_debug(arch; ultrashort = false)
     # and finally set the "true" initial condition with noise,
 
     set!(model, uh = uhⁱ, h = h̄)
+    set!(mean_h,mean(h))
 
     stop_time = if ultrashort 
         100
@@ -94,18 +82,6 @@ function run_shallow_simulation_debug(arch; ultrashort = false)
     #Create the simulation
     #simulation = Simulation(model, Δt = 1e-2, stop_time = 150)
     simulation = Simulation(model; Δt = 5.0, stop_time)
-
-    function update_convective_helper_arrays(sim, parameters)
-        p = parameters
-        #@info "Go run update_...!"
-        m = sim.model
-        update_convective_events!(m.architecture,p.isconvecting,p.convection_triggered_time,m.solution.h,
-                                  m.clock.time,p.τ_c,p.h_c,m.grid.Nx,m.grid.Ny, p.boundary_layer)
-        Oceananigans.BoundaryConditions.fill_halo_regions!(p.isconvecting, m.architecture)
-        Oceananigans.BoundaryConditions.fill_halo_regions!(p.convection_triggered_time, m.architecture)
-
-
-    end
 
     function progress(sim)
         m = sim.model
