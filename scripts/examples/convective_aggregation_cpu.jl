79a231a0293a38560cbe3460047abc3e86538547
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-07-19 18:33:16 -0700

    updated parameters in excample

--- scripts/examples/convective_aggregation_cpu.jl
+++ scripts/examples/convective_aggregation_cpu.jl
@@ -1,27 +1,26 @@
 using RamirezReyes_ShallowWaterInFPlane
 
 parameters = Dict(
-    "architecture" => "CPU",
-    "output_filename" => "convective_self_aggregation_run_CPU",
+    "architecture" => "GPU",
+    "output_filename" => "convective_self_aggregation_run_GPU",
     "f" => 0.0,# coriolis parameter5e-4 #5e-4
     "g" => 9.8,#gravitational acceleration 9.8
     "convection_timescale" => 28800.0, #convective time scale
     "convection_critical_height"=> 130.0, #convection_triggering height
     "heating_amplitude"    => 1.0e8,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
-    "large_scale_forcing" => (1.12/3)*1.0e-8,#radiative cooling rate
+    "large_scale_forcing" => (1.12/3)*1.0e-5,#radiative cooling rate
     "convective_radius"    => 15_000, #convective radius
     "relaxation_parameter" => 1.0/(2*86400), #relaxation timescale
-    "relaxation_height" => 131.0, #height to relax to, if nothing, h relax to its mean
+    "relaxation_height" => nothing, #height to relax to, if nothing, h relax to its mean
     "Lx" => 1.5e6, #domain
     "Ly" => 1.5e6,
     "Lz" => 126.5,
-    "Nx" => 500,
-    "Ny" => 500,
+    "Nx" => 250,
+    "Ny" => 250,
     "boundary_layer" => true, #if true Lz=30 otherwise Lz = 41
     "simulation_length_in_days" => 86400.0*20,
     "output_interval_in_seconds" => 7200, 
-    "timestep_in_seconds" => 20.0,
+    "timestep_in_seconds" => 60.0,
 )
-using RamirezReyes_ShallowWaterInFPlane
 
 run_shallow_simulation(parameters)
--- scripts/examples/convective_aggregation_gpu.jl
+++ scripts/examples/convective_aggregation_gpu.jl
@@ -8,20 +8,20 @@ parameters = Dict(
     "convection_timescale" => 28800.0, #convective time scale
     "convection_critical_height"=> 130.0, #convection_triggering height
     "heating_amplitude"    => 1.0e8,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
-    "large_scale_forcing" => (1.12/3)*1.0e-8,#radiative cooling rate
+    "large_scale_forcing" => (1.12/3)*1.0e-5,#radiative cooling rate
     "convective_radius"    => 15_000, #convective radius
     "relaxation_parameter" => 1.0/(2*86400), #relaxation timescale
     "relaxation_height" => nothing, #height to relax to, if nothing, h relax to its mean
     "Lx" => 1.5e6, #domain
     "Ly" => 1.5e6,
     "Lz" => 126.5,
-    "Nx" => 500,
-    "Ny" => 500,
+    "Nx" => 250,
+    "Ny" => 250,
     "boundary_layer" => true, #if true Lz=30 otherwise Lz = 41
     "simulation_length_in_days" => 86400.0*20,
     "output_interval_in_seconds" => 7200, 
-    "timestep_in_seconds" => 20.0,
+    "timestep_in_seconds" => 60.0,
 )
-using RamirezReyes_ShallowWaterInFPlane
+
 
 run_shallow_simulation(parameters)
