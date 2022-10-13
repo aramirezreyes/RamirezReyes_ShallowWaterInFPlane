fb4ca7046136b5ecbc5cca0ca2ea65a52dce8159
Argel Ramirez Reyes <aramirezreyes@ucdavis.edu>
2022-10-13 13:35:20 -0700

    Improved default updraft density in iorg

--- src/DataAnalysis/iorg.jl
+++ src/DataAnalysis/iorg.jl
@@ -5,9 +5,10 @@ Receive a 2-d array of Bool. Each `true` represents an updraft. Return an array 
 """
 function detect_updraft_clusters(array)
     connected = label_components(array, ones(Bool,3,3))
-    nclusters = 1:maximum(connected)
-    clustered = [findall(==(cluster), connected) for cluster in nclusters]
-    return clustered
+    nclusters = maximum(connected)
+    cluster_labels = 1:nclusters
+    clustered = [findall(==(cluster), connected) for cluster in cluster_labels]
+    return clustered,nclusters
 end
 
 
@@ -47,6 +48,7 @@ function compute_distances!(distances,centroids,r)
             newdistance = compute_distance(point,neighbor)
             distance = newdistance < distance ? newdistance : distance
         end
+        distance == Inf && continue
         distance_index = ceil(Int,distance/dr)
         distance_index > r.len && continue
         distances[distance_index] += 1
@@ -65,37 +67,64 @@ If updraft_density is not passed, this assumes that the simulation is in steady 
 Tompkins, A. M., & Semie, A. G. (2017). Organization of tropical convection in low vertical wind shears: Role of updraft entrainment. Journal of Advances in Modeling Earth Systems, 9(2), 1046–1068. https://doi.org/10.1002/2016MS000802
 
 """
-function compute_iorg(array::AbstractArray{Bool,3},x,y,Lx,Ly, updraft_density = count(array[:,:,end])/(Lx*Ly)) 
-    ## Next r_max comes from trying to estimate a radius to which the NNCDF of poisson-distributed points comes close to 1.0 for a given mean density 
+function compute_iorg(array::AbstractArray{Bool,3},x,y,Lx,Ly, updraft_density = 0.0) 
+    ## Next r_max comes from trying to estimate a radius to which the NNCDF of poisson-distributed points comes close to 1.0 for a given mean density
+    if iszero(updraft_density)
+        updraft_density = detect_updraft_clusters(array[:,:,end])[2] / (Lx*Ly)
+    end
     rmax = sqrt(-1 * log(0.001) /(updraft_density * π))
     r = range(0, stop = rmax,length = 100)
     nncdf_poisson = nncdf_poisson_func.(updraft_density,r)
     distances_count = zeros(Int,r.len)
+    nclusters = 0
     for arr_2d in eachslice(array,dims=3)
-        clusters = detect_updraft_clusters(arr_2d)
+        clusters,nclusters_local = detect_updraft_clusters(arr_2d)
+        if nclusters_local <= 1
+            continue
+        end
         centroids = map(cluster -> find_cluster_centroid(cluster,x, y), clusters)
         compute_distances!(distances_count,centroids,r)
+        nclusters = nclusters + nclusters_local
     end
-    nncdf_data_func = ecdf(r,weights = distances_count)
+    if nclusters <= 1
+        @info "Array needs to have at least 2 convective events in at least one frame! Returning NaN"
+        return distances_count, r, distances_count, nncdf_poisson, NaN
+    end
     #npoints unit area
-    nncdf_data = nncdf_data_func.(r)
+    nncdf_data_func = ecdf(r,weights = distances_count)
+    nncdf_data = if iszero(distances_count)
+        distances_count
+    else
+        nncdf_data = nncdf_data_func.(r)
+    end
     iorg = 0.5*sum((nncdf_data[1:end-1] .+ nncdf_data[2:end]) .* (nncdf_poisson[2:end] .- nncdf_poisson[1:end-1]))
     return distances_count,r,nncdf_data, nncdf_poisson, iorg
 end
 
 
-function compute_iorg(array::AbstractArray{Bool,2},x,y,Lx,Ly, updraft_density = count(array[:,:])/(Lx*Ly)) 
+function compute_iorg(array::AbstractArray{Bool,2},x,y,Lx,Ly, updraft_density = 0.0) 
     ## Next r_max comes from trying to estimate a radius to which the NNCDF of poisson-distributed points comes close to 1.0 for a given mean density 
+    clusters,nclusters = detect_updraft_clusters(array)
+    if iszero(updraft_density)
+        updraft_density = nclusters / (Lx*Ly)
+    end
     rmax = sqrt(-1 * log(0.001) /(updraft_density * π))
     r = range(0, stop = rmax,length = 100)
     nncdf_poisson = nncdf_poisson_func.(updraft_density,r)
     distances_count = zeros(Int,r.len)
-    clusters = detect_updraft_clusters(array)
+    if nclusters <= 1
+        @info "Array needs to have at least 2 convective events! Returning NaN"
+        return distances_count, r, distances_count, nncdf_poisson, NaN
+    end
     centroids = map(cluster -> find_cluster_centroid(cluster,x, y), clusters)
     compute_distances!(distances_count,centroids,r)
-    nncdf_data_func = ecdf(r,weights = distances_count)
     #npoints unit area
-    nncdf_data = nncdf_data_func.(r)
+    nncdf_data_func = ecdf(r,weights = distances_count)
+    nncdf_data = if iszero(distances_count)
+        distances_count
+    else
+        nncdf_data = nncdf_data_func.(r)
+    end
     iorg = 0.5*sum((nncdf_data[1:end-1] .+ nncdf_data[2:end]) .* (nncdf_poisson[2:end] .- nncdf_poisson[1:end-1]))
     return distances_count,r,nncdf_data, nncdf_poisson, iorg
 end
