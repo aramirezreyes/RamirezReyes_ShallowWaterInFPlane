ba5b4ae7a0c575f590b572b7c1bf0ef16f5e8a67
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2023-04-07 14:50:44 -0700

    changed vary_coriolis to use reference experiment

--- src/arr_dissertation/ref_experiment.jl
+++ src/arr_dissertation/ref_experiment.jl
@@ -12,7 +12,7 @@ function create_reference_experiment()
     "g" => 10.0,#gravitational acceleration 9.8
     "convection_timescale" => 14400.0, #convective time scale
     "convection_critical_height"=> 40.0, #convection_triggering height
-    "heating_amplitude"    => 5e7,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
+    "heating_amplitude"    => 5e8,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective heating
     "large_scale_forcing" => Derived("heating_amplitude", x -> x/1.3392857142857142e14),#radiative cooling rate corresponding to pair q=5e7, r = 1.12/3*1.0e-6
     "convective_radius"    => 30_000.0, #convective radius
     "relaxation_parameter" => 1.0/(0.8*86400.0), #relaxation timescale
@@ -25,7 +25,7 @@ function create_reference_experiment()
     "initialization_amplitude" => 0.01,
     "Lz" => Derived(["boundary_layer","convection_critical_height","initialization_amplitude"], lz),
     "simulation_length_in_days" => 200,
-    "output_interval_in_seconds" => 42300.0, 
+    "output_interval_in_seconds" => 43200.0, 
     "timestep_in_seconds" => 60.0,
     "relaxation_height" => nothing,
     "restart" => false,
