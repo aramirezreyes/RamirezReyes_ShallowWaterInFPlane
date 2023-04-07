ad82e80c511ecc9452845bdce1227971fb4279e4
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2023-04-07 11:10:24 -0700

    cleanup

--- scripts/vary_coriolis_useslurm.jl
+++ scripts/vary_coriolis_useslurm.jl
@@ -11,14 +11,14 @@ lz(bl, hc, ampl) = bl ? hc - ampl : hc + ampl
 
 parameter_space = Dict(
     "architecture" => "GPU",
-    "f" => [1e-6, 2e-6, 4e-6, 8e-6, 1e-5, 2e-5, 4e-5, 8e-5],# coriolis parameter5e-4 #5e-4                                                                                                    
-    "g" => 10.0,#gravitational acceleration 9.8                                                                                                  
-    "convection_timescale" => 14400.0, #convective time scale                                                                                    
-    "convection_critical_height"=> 40.0, #convection_triggering height                                                                           
-    "heating_amplitude"    => 5e8,#convective heating 1.0e9 #originally 9 for heating, -8 for cooling convective \heating                                                                                                                                                                   
-    "large_scale_forcing" => Derived("heating_amplitude", x -> x/1.3392857142857142e14),#radiative cooling rate cresponding to pair q=5e7, r = 1.12/3*1.0e-6                                                                                                                                     
-    "convective_radius"    => 30_000.0, #convective radius                                                                                       
-    "relaxation_parameter" => 1.0/(0.8*86400.0), #relaxation timescale                                                                           
+    "f" => [1e-6, 2e-6, 4e-6, 8e-6, 1e-5, 2e-5, 4e-5, 8e-5],                                                                                                 
+    "g" => 10.0,#gravitational acceleration                                                                                                 
+    "convection_timescale" => 14400.0,                                                                                 
+    "convection_critical_height"=> 40.0,                                                                          
+    "heating_amplitude"    => 5e8,                                                                                                                                                                 
+    "large_scale_forcing" => Derived("heating_amplitude", x -> x/1.3392857142857142e14),# radiative cooling rate cresponding to pair q=5e7, r = 1.12/3*1.0e-6                                                                                                                                     
+    "convective_radius"    => 30_000.0,                                                                                       
+    "relaxation_parameter" => 1.0/(0.8*86400.0), # 1/relaxation timescale                                                                           
     "Lx" => 48e6, #domain                                                                                                                        
     "Ly" => 48e6,
     "Nx" => 6000,
